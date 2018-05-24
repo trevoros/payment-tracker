c752e40d16e31422fb7a2244e0b475cc51131b36
Rosmel <rosmel@gmail.com>
2018-05-24 00:29:45 -0400

    update stuff

--- src/spabackendfunctionapp/GetPayments/index.js
+++ src/spabackendfunctionapp/GetPayments/index.js
@@ -1,6 +1,8 @@
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    context.log(`Http trigger state passed in: ${context.bindingData.state}`);
+
     if (context.bindings.inputDocuments && context.bindings.inputDocuments.length > 0) {
         context.res = {
             body: context.bindings.inputDocuments
